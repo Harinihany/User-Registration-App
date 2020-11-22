efaa83faaac397b24a6131c9cab5df6c14f30d91
Harinihany <harinihany03@gmail.com>
2020-11-22 16:16:32 +0530

    added: code of pattern matching for first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,12 @@
 
 echo "Welcome to User Registration Portal "
+
+echo "First Name : "
+read name
+pat="^[A-Z a-z]{3,}"
+if [[ $name =~ $pat ]]
+then
+	echo "$name is valid name"
+else
+	echo "not valid name"
+fi
